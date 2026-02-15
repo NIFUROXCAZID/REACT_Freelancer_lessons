f20722db634fe46f5ff591bd6eb3cfb946caf808
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2026-02-15 17:32:19 +0200

    Add files via upload

---
+++ test-app-1/src/components/task-1/index.js
@@ -0,0 +1,4 @@
+export { default } from './task-1'
+
+export { JustLorem2 } from './task-1'
+export { JustLorem3 } from './task-1'
---
+++ test-app-1/src/components/task-2/task-2.jsx
@@ -0,0 +1,6 @@
+export default function task2() {
+
+    return (
+        <p>task222222222222222222222 task2 task2 task2</p>
+    )
+}
