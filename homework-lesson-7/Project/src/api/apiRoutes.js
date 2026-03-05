2a844723d2607394dd861473619ab301dfdec024
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2026-03-05 19:18:42 +0200

    Update apiRoutes.js

--- homework-lesson-7/Project/src/api/apiRoutes.js
+++ homework-lesson-7/Project/src/api/apiRoutes.js
@@ -3,11 +3,12 @@ const backendUrl = import.meta.env.VITE_API_URL
 // Це обєкти url потім впишем потрібні
 export default {
     // getProductsByCategory: (category) => `${backendUrl}/products?category=${category}`,
-    productsList: `${backendUrl}/products`,
+    productsList: `${backendUrl}/api/products`,
     // addProduct: `${backendUrl}/products`,
     // getUpdateProductLink: (id) => `${backendUrl}/products/${id}`,
     // getProductById: (id) => `${backendUrl}/products/${id}`,
     // getDeleteProductLink: (id) => `${backendUrl}/products/${id}`,
 }
+
 
 
