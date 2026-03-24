44813e69f703b86bf3e419406cb092d50e364de6
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2026-03-24 13:04:46 +0200

    Update postsApi.js

--- homework-lesson-13/posts_front_lec/src/api/postsApi.js
+++ homework-lesson-13/posts_front_lec/src/api/postsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const postsApi = createApi({
   reducerPath: "postsApi",
   // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
   tagTypes: ["Post", "Posts"],
   endpoints: (build) => ({
     // Список постів з пагінацією (build.query)
