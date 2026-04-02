70ad19eb04797230911f06a889f75804acb78f7e
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2026-04-02 22:46:02 +0300

    Update server.js

--- homework-lesson-17/beckend_auth/server.js
+++ homework-lesson-17/beckend_auth/server.js
@@ -19,7 +19,7 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'https://react-homework-lesson-17.netlify.app', // 👈 ДОДАЙ ЦЕ
+      'https://react-homework-lesson-17.netlify.app/api/', // 👈 ДОДАЙ ЦЕ
     ],
     credentials: true,
   })
