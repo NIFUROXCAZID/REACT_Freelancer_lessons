1a9eca3b6803f36eb74b3c13b384c0bc1b574f1e
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2026-05-07 15:09:59 +0300

    Delete kfc-site/dist directory

--- kfc-site/dist/assets/HomePage-CED1jlyj.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Dw28XCpJ.js";function o(){return e.jsx("h1",{children:"Вітаємо у Firebase Shop!"})}export{o as default};
--- kfc-site/dist/assets/NotFoundPage-CGguGUdJ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dw28XCpJ.js";function r(){return t.jsx("h1",{children:"Сторінку не знайдено"})}export{r as default};
